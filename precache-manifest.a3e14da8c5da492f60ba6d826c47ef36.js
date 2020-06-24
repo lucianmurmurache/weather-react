1b2c4a7d6389f0404b8c296de98959a5fe1c4ef9
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-06-24 20:54:14 +0200

    Updates

---
+++ precache-manifest.a3e14da8c5da492f60ba6d826c47ef36.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed38b43443b17874bfc5895c0e267027",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "2ea23a0c8b7eee16ebc7",
+    "url": "/weather-react/static/css/main.08d43df2.chunk.css"
+  },
+  {
+    "revision": "981c26a76db8004994c5",
+    "url": "/weather-react/static/js/2.ab957aef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.ab957aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea23a0c8b7eee16ebc7",
+    "url": "/weather-react/static/js/main.df6968db.chunk.js"
+  },
+  {
+    "revision": "27042864e8d40029aeb9",
+    "url": "/weather-react/static/js/runtime-main.ed96b400.js"
+  },
+  {
+    "revision": "a143a34a812b8fef6d68df28c889c80b",
+    "url": "/weather-react/static/media/app.a143a34a.jpg"
+  },
+  {
+    "revision": "c8841e3474ed08e1f93f16f6c98ccf6d",
+    "url": "/weather-react/static/media/cold.c8841e34.jpg"
+  },
+  {
+    "revision": "44de4288c22121b0535fc9ae62d79acc",
+    "url": "/weather-react/static/media/hot.44de4288.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.3af9ff2bece374007aa57ca5ee2c4867.js"
+  "/weather-react/precache-manifest.a3e14da8c5da492f60ba6d826c47ef36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
